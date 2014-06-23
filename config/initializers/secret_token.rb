135439a374acdf1c3626f33578f0b0420a468500
Shaan Sapra <ssapra@uchicago.edu>
2014-06-22 18:35:29 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Edge::Application.config.secret_key_base = '77b30b47c1397f0c8c705fa6f1245a2fab8b8d799891cf43eb3038df97bcc080d252a25f09030689d792819410f0c2a6e1fd8ac9316de85621162f17b0c73992'
